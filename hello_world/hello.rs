0e7dd14c8b79b6b177cfaa17f5ca44ba7c415559
Tyler Kuromiya Parker <tylerkkp@gmail.com>
2020-11-26 22:23:46 -0800

    Hello World

---
+++ hello_world/hello.rs
@@ -0,0 +1,14 @@
+// Comments in Rust begin with '//'
+fn main () {
+    // println! is a macro that prints text to the console
+    println!("Hello World!");
+    println!("I'm a Rustacean!")
+}
+
+// Note: Use rustc to compile the binary in a terminal window
+// rustc hello.rs
+// The above line yields an executable called 'hello'
+// The executable can be run from terminal by calling ./hello
+// When run, 'hello' outputs the below to the terminal:
+// Hello World!
+// I'm a Rustacean!
