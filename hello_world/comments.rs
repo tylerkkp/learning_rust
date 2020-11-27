e758d66dbf61dd7a9bae237aad399e5badcead3a
Tyler Kuromiya Parker <tylerkkp@gmail.com>
2020-11-26 23:08:36 -0800

    Comments.rs

---
+++ hello_world/comments.rs
@@ -0,0 +1,39 @@
+// is a line comment
+/* is a block comment, and is closed with */
+/// is a line comment which will be parsed into HTML documentation
+//! is a block comment which will be parsed into HTML documentation
+
+/*
+The below is copy/pasted from the comments tutorial page
+I saw no need to retype, but will keep here for reference
+*/
+
+fn main() {
+    // This is an example of a line comment
+    // There are two slashes at the beginning of the line
+    // And nothing written inside these will be read by the compiler
+
+    // println!("Hello, world!");
+
+    // Run it. See? Now try deleting the two slashes, and run it again.
+
+    /*
+     * This is another type of comment, a block comment. In general,
+     * line comments are the recommended comment style. But
+     * block comments are extremely useful for temporarily disabling
+     * chunks of code. /* Block comments can be /* nested, */ */
+     * so it takes only a few keystrokes to comment out everything
+     * in this main() function. /*/*/* Try it yourself! */*/*/
+     */
+
+    /*
+    Note: The previous column of `*` was entirely for style. There's
+    no actual need for it.
+    */
+
+    // You can manipulate expressions more easily with block comments
+    // than with line comments. Try deleting the comment delimiters
+    // to change the result:
+    let x = 5 + /* 90 + */ 5;
+    println!("Is `x` 10 or 100? x = {}", x);
+}
